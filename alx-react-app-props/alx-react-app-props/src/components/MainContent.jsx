2f0108c4905e5d7e05a446058306dc60840a0a65
Salma <Salmaa_1781@hotmail.com>
2024-11-10 15:05:50 +0300

    new updates

---
+++ alx-react-app-props/alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main style={{ padding: '10px', textAlign: 'center' }}>
+            <p style={{ fontSize: '18px', fontWeight: 'bold' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
