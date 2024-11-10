6ba617ac02c97e1f0ae5e9e561e21134e66b7195
Salma <Salmaa_1781@hotmail.com>
2024-11-10 13:54:09 +0300

    Apply Inline CSS Styling to React Components

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-        <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+        <header style={{ backgroundColo: 'navy', color: 'white', textAlign: 'center' }}>
             <h1>My Favorite Cities</h1>
         </header>
     );
