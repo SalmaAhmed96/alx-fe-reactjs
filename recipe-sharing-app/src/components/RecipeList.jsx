c5bdcd83f73108ed15a0a9860db7f68aba3fac30
Salma <Salmaa_1781@hotmail.com>
2024-11-17 11:12:24 +0300

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import useRecipeStore from '../recipeStore';
 
 const RecipeList = () => {
@@ -16,6 +17,7 @@ const RecipeList = () => {
                 <div key={recipe.id}>
                     <h3>{recipe.title}</h3>
                     <p>{recipe.description}</p>
+                    <Link to={`/recipe/${recipe.id}`}>View Details</Link>
                 </div>
             ))}
         </div>
