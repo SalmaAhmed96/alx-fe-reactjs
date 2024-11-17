db3afdeb0b26b43badaf8e01a6c8b550c2721d45
Salma <Salmaa_1781@hotmail.com>
2024-11-17 11:22:28 +0300

    New Updates

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,20 +2,58 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
     recipes: [],
+    searchTerm: '',
+    filteredRecipes: [],
     favorites: [],
+    recommendations: [],
+
+
+    addRecipe: (newRecipe) => set((state) => ({
+        recipes: [...state.recipes, newRecipe]
+    })),
+
+
+    deleteRecipe: (id) => set((state) => ({
+        recipes: state.recipes.filter(recipe => recipe.id !== id)
+    })),
+
+
+    updateRecipe: (updatedRecipe) => set((state) => ({
+        recipes: state.recipes.map(recipe =>
+            recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+        )
+    })),
+
+
+    setSearchTerm: (term) => set({ searchTerm: term }),
+
+
+    filterRecipes: () => set((state) => ({
+        filteredRecipes: state.recipes.filter(recipe =>
+            recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+        )
+    })),
+
+
     addFavorite: (recipeId) => set((state) => ({
         favorites: [...state.favorites, recipeId]
     })),
+
+
     removeFavorite: (recipeId) => set((state) => ({
         favorites: state.favorites.filter(id => id !== recipeId)
     })),
-    recommendations: [],
+
+
     generateRecommendations: () => set((state) => {
         const recommended = state.recipes.filter(recipe =>
             state.favorites.includes(recipe.id) && Math.random() > 0.5
         );
         return { recommendations: recommended };
-    })
+    }),
+
+
+    setRecipes: (recipes) => set({ recipes })
 }));
 
 export default useRecipeStore;
