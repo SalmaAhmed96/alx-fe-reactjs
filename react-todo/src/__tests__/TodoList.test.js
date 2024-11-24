6d2275612fd0bf8d8cca38b0dd52b7e05658b2b2
Salma <Salmaa_1781@hotmail.com>
2024-11-25 00:41:17 +0300

    Implementing and Testing a Todo List Component in React

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,24 +1,25 @@
-import React from 'react';
+/* eslint-env jest */
+
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import TodoList from '../components/TodoList';
 
 describe('TodoList Component', () => {
-  test('Write Initial Render Test', () => {
+  test('Initial Render Test', () => {
     render(<TodoList />);
     expect(screen.getByText('Todo List')).toBeInTheDocument();
     expect(screen.getByText('Learn React')).toBeInTheDocument();
     expect(screen.getByText('Learn Jest')).toBeInTheDocument();
   });
 
-  test('Test Adding Todos', () => {
+  test('Adding Todos', () => {
     render(<TodoList />);
     fireEvent.change(screen.getByRole('textbox'), { target: { value: 'New Todo' } });
     fireEvent.click(screen.getByText('Add Todo'));
     expect(screen.getByText('New Todo')).toBeInTheDocument();
   });
 
-  test('Test Toggling Todos', () => {
+  test('Toggling Todos', () => {
     render(<TodoList />);
     const todoItem = screen.getByText('Learn React');
     fireEvent.click(todoItem);
@@ -27,7 +28,7 @@ describe('TodoList Component', () => {
     expect(todoItem).toHaveStyle('text-decoration: none');
   });
 
-  test('Test Deleting Todos', () => {
+  test('Deleting Todos', () => {
     render(<TodoList />);
     const deleteButton = screen.getAllByText('Delete')[0];
     fireEvent.click(deleteButton);
