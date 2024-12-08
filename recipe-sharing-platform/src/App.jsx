f0821d05a65ece0a3a2c85df0bcd41a02b30be36
Salma <Salmaa_1781@hotmail.com>
2024-12-08 21:22:07 +0300

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-blue-500">
+      <h1>Recipe Sharing Platform</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
