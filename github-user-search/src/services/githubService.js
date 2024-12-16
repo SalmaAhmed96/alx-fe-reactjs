8d8f0794d840fc38de6a54482649b6d648dadb52
Salma <Salmaa_1781@hotmail.com>
2024-12-16 04:07:10 +0300

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const API_URL = 'https://api.github.com/search/users?q=';
 const API_KEY = import.meta.env.VITE_GITHUB_API_KEY;
 
-export const fetchUserData = async ({ username, location, repos }) => {
+export const fetchUserData = async ({ username, location, minRepos }) => {
   try {
     let query = username ? `${username}` : '';
     if (location) query += `+location:${location}`;
-    if (repos) query += `+repos:>=${repos}`;
+    if (minRepos) query += `+repos:>=${minRepos}`;
 
     const response = await axios.get(`${API_URL}${query}`, {
       headers: {
